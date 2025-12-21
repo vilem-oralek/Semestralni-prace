fa39544c19a054d6f3fd83266fef69ceaf7ee680
oralevil <vilem.oralek@Vilem--Notebook.local>
2025-12-21 16:58:06 +0100

    vytvoření PHP dokumentace

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\calculateTotalPrice\u0028\u0029",
+            "name": "calculateTotalPrice",
+            "summary": "Vypo\u010D\u00EDt\u00E1\u0020celkovou\u0020cenu\u0020rezervace\u0020na\u0020z\u00E1klad\u011B\u0020z\u00E1kladn\u00ED\u0020ceny\u0020a\u0020sez\u00F3nn\u00EDch\u0020cen.",
+            "url": "namespaces/default.html#function_calculateTotalPrice"
+        },                {
+            "fqsen": "\\isTermAvailable\u0028\u0029",
+            "name": "isTermAvailable",
+            "summary": "Kontrola,\u0020zda\u0020je\u0020po\u017Eadovan\u00FD\u0020term\u00EDn\u0020dostupn\u00FD.",
+            "url": "namespaces/default.html#function_isTermAvailable"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
